400c009a6377ad761812753ebd7d9f111d39ba6f
DanSantana <dangelosantana@hotmail.com>
2023-01-09 17:19:01 -0500

    first issue

---
+++ EFCoreWebAPIEntityReview/Models/Heroi.cs
@@ -0,0 +1,15 @@
+﻿namespace EFCoreWebAPIEntityReview.Models
+{
+    public class Heroi
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public IdentidadeSecreta IdentidadeSecreta { get; set; }
+
+        public List<Arma> Armas { get; set; }
+        public List<HeroiBatalha> HeroisBatalhas { get; set; }
+
+    }
+}
